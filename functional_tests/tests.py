029e4498af274a59f10b2b71c5cffc800a08e6d7
Ghios <56654758+Jbaker16163@users.noreply.github.com>
2020-12-09 23:02:15 -0500

    5 changed files URLs for forms x2, moved code in views + tests, new URL

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -4,16 +4,17 @@ from selenium.webdriver.common.keys import Keys
 import time
 import unittest
 from selenium.common.exceptions import WebDriverException
+from django.test import LiveServerTestCase
 
-class NewVisitorTest(unittest.TestCase):
+MAX_WAIT = 10
+
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
 
     def tearDown(self):
         self.browser.quit()
-
-    MAX_WAIT = 10
 
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
