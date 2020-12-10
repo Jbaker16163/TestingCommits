54b0f74e8aaa57e457ff079a04c0a8642926c278
Ghios <56654758+Jbaker16163@users.noreply.github.com>
2020-12-10 00:06:35 -0500

    new URL + view for adding to existing lists. FT passes :-)

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -68,11 +68,6 @@ class NewVisitorTest(LiveServerTestCase):
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
         self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
         
-
-        # Edith wonders whether the site will remember her list. Then she sees
-        # that the site has generated a unique URL for her -- there is some
-        # explanatory text to that effect.
-        self.fail('Finish the test!')
         
     def test_multiple_users_can_start_lists_at_different_urls(self):
         # Edith starts a new to-do list
