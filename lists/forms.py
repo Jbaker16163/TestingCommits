c5b99f8beb0fe6ca0ad57fe2e5bf319ceb0cfc2c
Ghios <56654758+Jbaker16163@users.noreply.github.com>
2021-01-05 11:52:17 -0500

    Final Commit

--- lists/forms.py
+++ lists/forms.py
@@ -20,9 +20,8 @@ class ItemForm(forms.models.ModelForm):
             'text': {'required': EMPTY_ITEM_ERROR}
         }
 
-    def save(self, for_list):
-        self.instance.list = for_list
-        return super().save()
+    def save(self):
+        return forms.models.ModelForm.save(self)
     
 class ExistingListItemForm(ItemForm):
 
@@ -37,3 +36,5 @@ class ExistingListItemForm(ItemForm):
         except ValidationError as e:
             e.error_dict = {'text': [DUPLICATE_ITEM_ERROR]}
             self._update_errors(e)
+    
+    
